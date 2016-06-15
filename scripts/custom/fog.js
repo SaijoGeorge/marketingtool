848447a17d7b81375e22ecb6f2138590ee2554fa
Saijo George <ppcinformer@gmail.com>
2016-06-15 14:25:35 +1000

    fog density

--- scripts/custom/fog.js
+++ scripts/custom/fog.js
@@ -253,7 +253,7 @@ var bgi = 'assets/images/mybackground.png';
 new Fog({
     selector: '#fog',
     particle: 'assets/images/fog-particle.png',
-    density: 80,
+    density: 150,
     bgi: bgi
 });
 
